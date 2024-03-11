06a9eee0b06caf75e289e9c012ba977a5455ebd3
mariam.sanoko <mariam.sanoko@gmail.com>
2024-03-11 16:14:22 +0100

    Update DefaultController.php

--- mentorHub/src/Controller/DefaultController.php
+++ mentorHub/src/Controller/DefaultController.php
@@ -1,33 +1,22 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
-{
-    #[Route('/')]
-    public function home()
 
+    #[Route('/search/{specialite}')]
+    # {slug} représente un paramètre de la route.
+    public function searchSpecialite($specialite)
     {
-        return $this->render(view: 'default/home.html.twig');
+        # TODO Rechercher dans ma BDD tous les medecins qui font cette spécialité.
+        # TODO Retourner le résultat dans ma vue, pour affichage...
+
+        return new Response("
+            <h1>Vous recherchez un spécialiste : $specialite</h1>
+        ");
     }
 
-    #[Route('/category/{slug}')]
-    public function category($slug)
-    {
-        return $this->render('default/category.html.twig', [
-            'slug' => $slug,
-        ]);
-    }
 }
-    #[Route('/page/contact.html')]
-    # Ex. http://127.0.0.1:8000/page/contact.html
-    public function contact()
-    {
-        return new Response('
-            <h1>Contactez-nous</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolorem itaque repudiandae. At consequuntur debitis expedita fuga fugit harum mollitia numquam odio quia quis? Dignissimos ducimus eius id nemo quibusdam.</p>
-        ');
-    }
