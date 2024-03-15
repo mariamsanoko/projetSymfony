29567d9649a1113cc22d4ff065f2101733bc8737
mariam.sanoko <mariam.sanoko@gmail.com>
2024-03-15 15:50:01 +0100

    Revert "Correction version PHP"
    This reverts commit 08b32ea5aeed6521f3f8d4b7cf8335e938af38fc.

---
+++ mentorHub/src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route(path: '/register.html', name: 'register')]
+    public function register()
+    {
+        #create user 'vide'
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+}
