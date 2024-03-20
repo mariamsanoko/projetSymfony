7fa7e9a14208818e9cd8cccfe51d5f8e7b50bded
mariam.sanoko <mariam.sanoko@gmail.com>
2024-03-20 22:34:39 +0100

    Update UserController.php

--- mentorHub/src/Controller/Controller/UserController.php
+++ mentorHub/src/Controller/Controller/UserController.php
@@ -7,11 +7,14 @@ use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {   #[Route('inscription.html')]
-    public function register(Request $request, EntityManagerInterface $manager)
+    public function register(Request $request,
+                             UserPasswordHasherInterface $hasher,
+                             EntityManagerInterface $manager)
     {
         #dd($request);
         # verify data the send to form
@@ -27,18 +30,25 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         # is submitted by user
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
                 #dd($user);
 
-            # save in bbd
+            // Encode de password
+            $hashedPassword = $hasher->hashPassword($user, $user->getPassword());
+
+            // Update user mdp hashed
+            $user->setPassword($hashedPassword);
+
+            # Persist user in BDD
             $manager->persist($user);
             $manager->flush();
-            # Encode de password
+
             # Redirection
             return $this->redirectToRoute('app_default_home');
             }
 
         #Pass form to the view
+        // Read form inscription
 
         return $this->render('user/register.html.twig', [
             'form' => $form->createView(),
