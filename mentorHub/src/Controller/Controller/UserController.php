fb64e14a2b73bb69802c2b51756f0d2eaef2806f
mariam.sanoko <mariam.sanoko@gmail.com>
2024-03-20 17:23:22 +0100

    Creation Formulaire
    Formulaire inscription

---
+++ mentorHub/src/Controller/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{   #[Route('inscription.html')]
+    public function register()
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+}
--- mentorHub/src/Form/UserType.php
+++ mentorHub/src/Form/UserType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,14 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastName')
-            ->add('mentorsessions')
+            ->add('firstname', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+
+            ->add('submit', SubmitType::class)
+
+           
         ;
     }
 
