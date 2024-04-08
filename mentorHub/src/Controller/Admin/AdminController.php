1847b5d0bef6755ca877dfd89acc282fa0b79d98
mariam.sanoko <mariam.sanoko@gmail.com>
2024-04-08 20:37:37 +0200

    home admin tpl

--- mentorHub/src/Controller/Admin/AdminController.php
+++ mentorHub/src/Controller/Admin/AdminController.php
@@ -3,17 +3,13 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin')]
-/*
- * @Route("/admin")
- * class Admin Controller
- * @package App\Controller\Admin
- * */
+
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/home')]
+    #[Route('/home', name: 'admin')]
     public function admin()
     {
         return  $this->render("admin/home.html.twig");
