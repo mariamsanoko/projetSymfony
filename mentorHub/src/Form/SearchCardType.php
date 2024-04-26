f420f43d7058e3824cb940651e4fa3360b2a48b7
mariam.sanoko <mariam.sanoko@gmail.com>
2024-04-26 18:19:04 +0200

    test search

--- mentorHub/src/Form/SearchCardType.php
+++ mentorHub/src/Form/SearchCardType.php
@@ -9,15 +9,22 @@ class SearchCardType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
 
-    {
-        $builder
-        ->add('category', ChoiceType::class,[
+    {   
+        // define course categories 
+        $courses = ['course_1', 'course_2', 'course_3'];
+        $categories = ['category_1', 'category_2','categories_3'];
+
+        $courseChoices = array_combine($courses,$courses);
+        $categoryChoices = array_combine($categories,$categories)
+
+       
+        ->add('nameCourse', ChoiceType::class, [
             'choices' => [
-                'nameCourse'=>'nameCourse',
-                'mentorName'=>'mentorName',
-                'createdAt'=>'createdAt',
-               'courses'=>'courses',
-               //array_combine()
+                //'nameCourse'=>'nameCourse',
+                //'mentorName'=>'mentorName',
+                //'createdAt'=>'createdAt',
+               //'courses'=>'courses',
+              // array_combine($SearchCardType,$SearchCardType)
             ]
         ])
         ->add('category', ChoiceType::class,[
