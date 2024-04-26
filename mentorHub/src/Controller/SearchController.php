498b6798de7fbef56b8b7eaf1646679993029094
mariam.sanoko <mariam.sanoko@gmail.com>
2024-04-26 11:42:20 +0200

    SearchController

--- mentorHub/src/Controller/SearchController.php
+++ mentorHub/src/Controller/SearchController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
-    #[Route('/mentor/search', name: 'search_mentor')]
+    #[Route('/search/mentor/', name: 'search_mentor')]
     public function searchMentor(Request $request)
     {
         return $this->render('search/mentor.html.twig');
