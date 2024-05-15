25674ba95aa4e86f78c2c4ad2d2633cf4a7feabe
mariam.sanoko <mariam.sanoko@gmail.com>
2024-05-15 10:37:18 +0200

    bug fix

--- mentorHub/src/Controller/DefaultController.php
+++ mentorHub/src/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller; // Mettez le namespace correct correspondant au chemin de votre contrôleur
+namespace App\Controller;
 
 use App\Repository\CatalogRepository;
 use App\Repository\CategoryRepository;
@@ -19,7 +19,6 @@ class DefaultController extends AbstractController
      * findBy() :
      */
     #[Route('/')]
-    // Ex. http://127.0.0.1:8000/
     public function home(MentorRepository $mentorRepository): Response
     {
         return $this->render('default/home.html.twig', [
@@ -28,46 +27,34 @@ class DefaultController extends AbstractController
     }
 
     #[Route('/categorie/{id}')]
-    /* # Ex. http://127.0.0.1:8000/categorie/3
-     * # {slug} représente un paramètre de la route.*/
-    public function category($id, CategoryRepository $categoryRepository)
-
-    {   #recuprer la categorie via son ID
+    public function category($id, CategoryRepository $categoryRepository): Response
+    {
         $category = $categoryRepository->find($id);
-        # dd($category);
         return $this->render('default/category.html.twig', [
             'category' => $category,
         ]);
     }
 
-
     #[Route('/mentor/{slug}')]
-    /* # Ex. http://127.0.0.1:8000/categorie/3
-     * # {slug} représente un paramètre de la route.*/
-    public function mentor($slug, MentorRepository $mentorRepository)
-
-    {   #recuprer la categorie via son ID
-        $mentor = $mentorRepository->findOneBy(['slug'=> $slug]);
-        # dd($mentor);
-        return $this->render('default/category.html.twig', [
+    public function mentor($slug, MentorRepository $mentorRepository): Response
+    {
+        $mentor = $mentorRepository->findOneBy(['slug' => $slug]);
+        return $this->render('default/mentor.html.twig', [
             'mentor' => $mentor,
         ]);
     }
-    #[Route('/categories/{slug}')]
 
-    public function categories($slug)
+    #[Route('/categories/{slug}')]
+    public function categories($slug): Response
     {
         return $this->render('default/categories.html.twig', [
             'slug' => $slug
         ]);
-
     }
-    
-    #[Route('/contact.html')]
 
-    public function contact()
+    #[Route('/contact.html')]
+    public function contact(): Response
     {
         return $this->render('contact/contact.html.twig');
     }
-
 }
